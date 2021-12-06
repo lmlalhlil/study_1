144cc22111afc548678caf3c03117f2dcf91b801
maedeh <emamimaedeh7481@gmail.com>
2021-12-06 10:01:17 +0330

    finish

--- study_1/Source.cpp
+++ study_1/Source.cpp
@@ -8,28 +8,37 @@ using std::map;
 #include <string>
 using std::string;
 
+class Exam {
+public:
+    
+    void test_1() { cout << "Write the answers to the following questions?\n"; }
+    void test_2() {cout << "2+2=\n";}
+    void test_3() { cout << "5_3=\n"; }
+};
+
 int main() {
 
-    cout << "welcom\n" << endl;
-
     map <int, string> name;
-    name[0] = "zara";
-    name[1] = "afra";
-    name[2] = "neda";
-    name[3] = "taraneh";
-    name[4] = "maryam";
+    name[0] = "zara\n";
+    name[1] = "afra\n";
+    name[2] = "neda\n";
+    name[3] = "taraneh\n";
+    name[4] = "maryam\n";
 
-    vector<int>age;
-    age[0] = 18;
-    age[1] = 22;
-    age[2] = 16;
-    age[3] = 20;
-    age[4] = 21;
-
-    for (int i = 0; i < name.size(); i++)
+        for (int i = 0; i < name.size(); i++)
     {
-        cout << name[i] << "=" << age[i] << endl;
+        cout << name[i];
     }
+
+
+cout << "welcom\n" << endl;
+
+        Exam pass;
+    pass.test_1();
+    pass.test_2();
+    pass.test_3();
+
+    cout << endl << "Good luck";
 
 
     return 0;
